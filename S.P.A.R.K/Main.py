a282fec5d49891a66c7975b92313eb8d8f098c84
Fanlitecra <154455828+Fanlitecra@users.noreply.github.com>
2025-10-29 09:52:58 -0300

    Add files via upload
    Acá está el folder de S.P.A.R.K

---
+++ S.P.A.R.K/Main.py
@@ -0,0 +1,57 @@
+import sys
+from PySide6.QtGui import QPixmap
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QSizePolicy,
+    QStatusBar, QWidget)
+
+"""
+from Archivo convertido con pyside2-uic archivo.ui > interfaz.py
+import nombre de la clase del archivo convertido
+"""
+from UI_Spark import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Variables de ejemplo
+        self.Distancia = "50 cm"
+        self.Alerta = "Todo seguro ✅"
+
+        # Mostrar los valores en los labels
+        self.actualizar_labels()
+
+    def actualizar_labels(self):
+        # Usa setText() para cambiar el texto de los labels
+        self.ui.label.setText(f"Distancia: {self.Distancia}")
+        self.ui.label_2.setText(self.Alerta)
+
+print("""
+   _____ ____  ___    ____  __ __
+  / ___// __ \/   |  / __ \/ //_/
+  \__ \/ /_/ / /| | / /_/ / ,<   
+ ___/ / ____/ ___ |/ _, _/ /| |  
+/____/_/   /_/  |_/_/ |_/_/ |_|  
+Sensor de Proximidad Automático 
+    para Riesgos Kinéticos                             
+      """)
+
+
+
+
+
+if __name__ == "__main__": #checkea si el script está siendo ejecutado como el prog principal (no importado como un modulo).
+    app = QApplication(sys.argv)    # Crea un Qt widget, la cual va ser nuestra ventana.
+    window = MainWindow() #crea una intancia de MainWindow 
+    window.show() # IMPORTANT!!!!! la ventanas estan ocultas por defecto.
+    sys.exit(app.exec()) # Start the event loop.
+
+# Never gonna give you up
---
+++ S.P.A.R.K/UI_Spark.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'MainWindow - untitledYeuxwv.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QMenuBar,
+    QSizePolicy, QStatusBar, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(562, 559)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(60, 120, 161, 21))
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(60, 160, 201, 21))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 562, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+#if QT_CONFIG(shortcut)
+        self.label.setBuddy(self.label)
+        self.label_2.setBuddy(self.label_2)
+#endif // QT_CONFIG(shortcut)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+    # retranslateUi
